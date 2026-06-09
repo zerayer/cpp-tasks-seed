b9fcbeaa8b31696bae7629a5b5b586a4cdc5a381
zerayer <Zerayar@yandex.ru>
2026-06-09 04:39:14 +0400

    делаем

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,11 +1,113 @@
-#include <gtest/gtest.h>
+#include <algorithm>
+#include <vector>
 
-// А вот тут collvalue.h не надо, берите просто инты
+#include "gtest/gtest.h"
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
-
-TEST(FooSuite, FooTest)
+TEST(SortingSuite, BubbleSortEmpty)
 {
-    EXPECT_EQ(0, 0);
+    std::vector<int> data;
+
+    bubble_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(data.empty());
+}
+
+TEST(SortingSuite, HeapSortEmpty)
+{
+    std::vector<int> data;
+
+    heap_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(data.empty());
+}
+
+TEST(SortingSuite, BubbleSortOneElement)
+{
+    std::vector<int> data = {42};
+
+    bubble_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, HeapSortOneElement)
+{
+    std::vector<int> data = {42};
+
+    heap_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, BubbleSortSimple)
+{
+    std::vector<int> data = {5, 1, 4, 2, 8};
+
+    bubble_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, HeapSortSimple)
+{
+    std::vector<int> data = {5, 1, 4, 2, 8};
+
+    heap_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, BubbleSortWithRepeats)
+{
+    std::vector<int> data = {3, 1, 2, 3, 1, 2};
+
+    bubble_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, HeapSortWithRepeats)
+{
+    std::vector<int> data = {3, 1, 2, 3, 1, 2};
+
+    heap_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, BubbleSortAlreadySorted)
+{
+    std::vector<int> data = {1, 2, 3, 4, 5};
+
+    bubble_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, HeapSortAlreadySorted)
+{
+    std::vector<int> data = {1, 2, 3, 4, 5};
+
+    heap_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, BubbleSortReverseOrder)
+{
+    std::vector<int> data = {5, 4, 3, 2, 1};
+
+    bubble_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
+}
+
+TEST(SortingSuite, HeapSortReverseOrder)
+{
+    std::vector<int> data = {5, 4, 3, 2, 1};
+
+    heap_sort(data.begin(), data.end());
+
+    EXPECT_TRUE(std::is_sorted(data.begin(), data.end()));
 }
