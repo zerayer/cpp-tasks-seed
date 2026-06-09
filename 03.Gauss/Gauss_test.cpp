f8c39608603778df358131a60e5b2a9a37a18fe4
zerayer <Zerayar@yandex.ru>
2026-06-09 05:12:53 +0400

    Update Gauss_test.cpp 1

--- 03.Gauss/Gauss_test.cpp
+++ 03.Gauss/Gauss_test.cpp
@@ -1,6 +1,80 @@
-#include <gtest/gtest.h>
+#include <random>
+#include <stdexcept>
 
-TEST(GaussSolve, SmallSolve)
+#include "gtest/gtest.h"
+
+#include "Gauss_solve.h"
+
+TEST(GaussSolve, Small2x2)
 {
-    EXPECT_EQ(0, 0);
+    GaussMatrix ab(2, 3);
+    ab << 2, 1, 5,
+          1, 3, 7;
+
+    GaussVector x = Gauss_solve(ab);
+
+    EXPECT_NEAR(x(0), 1.6, 1e-9);
+    EXPECT_NEAR(x(1), 1.8, 1e-9);
+}
+
+TEST(GaussSolve, Classic3x3)
+{
+    GaussMatrix ab(3, 4);
+    ab << 2, 1, -1, 8,
+          -3, -1, 2, -11,
+          -2, 1, 2, -3;
+
+    GaussVector x = Gauss_solve(ab);
+
+    EXPECT_NEAR(x(0), 2.0, 1e-9);
+    EXPECT_NEAR(x(1), 3.0, 1e-9);
+    EXPECT_NEAR(x(2), -1.0, 1e-9);
+}
+
+TEST(GaussSolve, SingularMatrixThrows)
+{
+    GaussMatrix ab(2, 3);
+    ab << 1, 2, 3,
+          2, 4, 6;
+
+    EXPECT_THROW(Gauss_solve(ab), std::runtime_error);
+}
+
+TEST(GaussSolve, GeneratedLargeSystem)
+{
+    constexpr int n = 30;
+
+    std::mt19937 generator(42);
+    std::uniform_real_distribution<double> distribution(-10.0, 10.0);
+
+    GaussMatrix a(n, n);
+    GaussVector expected(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        expected(i) = distribution(generator);
+
+        for (int j = 0; j < n; ++j)
+        {
+            a(i, j) = distribution(generator);
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        a(i, i) += 100.0;
+    }
+
+    GaussVector b = a * expected;
+    GaussMatrix ab(n, n + 1);
+
+    ab.block(0, 0, n, n) = a;
+    ab.col(n) = b;
+
+    GaussVector actual = Gauss_solve(ab);
+
+    for (int i = 0; i < n; ++i)
+    {
+        EXPECT_NEAR(actual(i), expected(i), 1e-7);
+    }
 }
