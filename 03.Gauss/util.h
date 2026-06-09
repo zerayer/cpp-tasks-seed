27eb030bdcdb8984d85744450fe2ece19be1dae7
zerayer <Zerayar@yandex.ru>
2026-06-09 05:11:28 +0400

    Update util.h 1

--- 03.Gauss/util.h
+++ 03.Gauss/util.h
@@ -1,7 +1,15 @@
+#ifndef GAUSS_UTIL_H
+#define GAUSS_UTIL_H
+
+#include <iosfwd>
+
 #include <Eigen/Dense>
 
-typedef Eigen::VectorXd /* Eigen::Matrix<double, Eigen::Dynamic, 1> */ GaussVector;
-typedef Eigen::Matrix<double, -1, -1, Eigen::RowMajor> GaussMatrix;
+using GaussVector = Eigen::VectorXd;
+using GaussMatrix = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 
 GaussMatrix load_csv_to_matrix(const char *filename);
-void print_matrix_as_csv(std::ostream& out, const GaussMatrix &matrix, int prec = 6);
+
+void print_solution_csv(std::ostream& out, const GaussVector& solution, int prec = 6);
+
+#endif // GAUSS_UTIL_H
